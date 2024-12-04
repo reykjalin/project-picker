95b29fa5e592a8e81eb2e9de93d646bfb057c55b
Kristófer R. <kristofer@thorlaksson.com>
2024-12-03 20:09:43 -0500

    pp: 1.0.0
    Reads a list of project directories from
    ~/.config/project-picker/projects and presents the items in a list that
    can be filtered based on user input.
    
    Intended functionality is to feed this to something like `cd` to quickly
    jump between some predefined list of directories.

---
+++ README.md
@@ -0,0 +1,67 @@
+# Project picker
+
+Presents a filterable list of pre-defined strings from `~/.config/project-picker/projects` for you to select and provide to some shell command.
+Once an item in the list is selected `project-picker` will print the full path to STDOUT.
+
+**Exit codes:**
+
+* If an item is picked `project-picker` will exit with status `0`.
+* If no item is chosen `project-picker` will exit with status `1`.
+* If `Ctrl-C` is used to close `project-picker` instead of selecting an item it will exit with status `1`.
+* If an error occurred `project-picker` will exit with status `74` and an error message is printed to STDERR.
+
+## Usage example: jump between project folders
+
+Add a list of the projects you want easily available to a config file, then run `project-picker`.
+`project-picker` will print the selected project path to STDOUT.
+
+### Add project paths to the config file
+
+```
+~/projects/project-a
+~/projects/project-b
+/Users/jdoe/projects/project-c
+```
+
+### Example: jump between projects with cd
+
+Put in a shell script or alias:
+
+```fish
+# ~/.config/fish/functions/pp.fish
+# Fish alias for project-picker, usage: pp
+function pp
+  set dir (project-picker)
+
+  # A non-zero exit code means no project was selected.
+  if test $status -eq 0
+      cd $dir
+  end
+end
+```
+
+
+## Usage example: pass result to command
+
+This is the quick and dirty way to use `project-picker`.
+If an error occurs or you don't pick an item nothing will be passed to the command.
+
+```bash
+# `cd` to selected item.
+cd $(project-picker)
+
+# Open selected item in vim.
+vim $(project-picker)
+```
+
+
+## Build `project-picker`
+
+```sh
+# Will place `project-picker` in ./zig-out/bin
+zig build -Doptimize=ReleaseSafe
+
+# Will place `project-picker` in ~/.local/bin
+zig build -Doptimize=ReleaseSafe --prefix ~/.local
+```
+
---
+++ src/main.zig
@@ -0,0 +1,326 @@
+// Heavily based on the fuzzy example from libvaxis:
+// https://github.com/rockorager/libvaxis/blob/5a8112b78be7f8c52d7404a28d997f0638d1c665/examples/fuzzy.zig
+
+const std = @import("std");
+const kf = @import("known-folders");
+const vaxis = @import("vaxis");
+const vxfw = vaxis.vxfw;
+
+pub const known_folders_config: kf.KnownFolderConfig = .{
+    .xdg_on_mac = true,
+};
+
+const ProjectPicker = struct {
+    /// The full list of available items.
+    list: std.ArrayList(vxfw.Text),
+    /// The filtered list of available items.
+    filtered: std.ArrayList(vxfw.RichText),
+    /// The ListView used to render the filtered list of items.
+    list_view: vxfw.ListView,
+    /// The input box to type in a search pattern.
+    text_field: vxfw.TextField,
+
+    /// Used to allocate RichText widgets in the ListView.
+    arena: std.heap.ArenaAllocator,
+
+    /// Stores the selected path.
+    result: std.ArrayList(u8),
+
+    pub fn widget(self: *ProjectPicker) vxfw.Widget {
+        return .{
+            .userdata = self,
+            .eventHandler = ProjectPicker.typeErasedEventHandler,
+            .drawFn = ProjectPicker.typeErasedDrawFn,
+        };
+    }
+
+    pub fn eventHandler(
+        self: *ProjectPicker,
+        ctx: *vxfw.EventContext,
+        event: vxfw.Event,
+    ) anyerror!void {
+        switch (event) {
+            .init => {
+                // Initialize the filtered list
+                const allocator = self.arena.allocator();
+                for (self.list.items) |line| {
+                    var spans = std.ArrayList(vxfw.RichText.TextSpan).init(allocator);
+                    const span: vxfw.RichText.TextSpan = .{ .text = line.text };
+                    try spans.append(span);
+                    try self.filtered.append(.{ .text = spans.items });
+                }
+
+                return ctx.requestFocus(self.text_field.widget());
+            },
+            .key_press => |key| {
+                if (key.matches('c', .{ .ctrl = true })) {
+                    ctx.quit = true;
+                    return;
+                }
+
+                try self.list_view.handleEvent(ctx, event);
+            },
+            .focus_in => {
+                return ctx.requestFocus(self.text_field.widget());
+            },
+            else => {},
+        }
+    }
+
+    pub fn draw(
+        self: *ProjectPicker,
+        ctx: vxfw.DrawContext,
+    ) std.mem.Allocator.Error!vxfw.Surface {
+        const max = ctx.max.size();
+
+        var list_view: vxfw.SubSurface = .{
+            .origin = .{ .row = 2, .col = 0 },
+            .surface = try self.list_view.draw(ctx.withConstraints(
+                ctx.min,
+                .{ .width = max.width, .height = max.height - 3 },
+            )),
+        };
+        list_view.surface.focusable = false;
+
+        const text_field: vxfw.SubSurface = .{
+            .origin = .{ .row = 0, .col = 2 },
+            .surface = try self.text_field.draw(ctx.withConstraints(
+                ctx.min,
+                .{ .width = max.width, .height = 1 },
+            )),
+        };
+
+        const prompt: vxfw.Text = .{ .text = "", .style = .{ .fg = .{ .index = 4 } } };
+
+        const prompt_surface: vxfw.SubSurface = .{
+            .origin = .{ .row = 0, .col = 0 },
+            .surface = try prompt.draw(ctx.withConstraints(ctx.min, .{ .width = 2, .height = 1 })),
+        };
+
+        const children = try ctx.arena.alloc(vxfw.SubSurface, 3);
+        children[0] = list_view;
+        children[1] = text_field;
+        children[2] = prompt_surface;
+
+        return .{
+            .size = max,
+            .widget = self.widget(),
+            .focusable = true,
+            .buffer = &.{},
+            .children = children,
+        };
+    }
+
+    fn typeErasedEventHandler(
+        ptr: *anyopaque,
+        ctx: *vxfw.EventContext,
+        event: vxfw.Event,
+    ) anyerror!void {
+        const self: *ProjectPicker = @ptrCast(@alignCast(ptr));
+        try self.eventHandler(ctx, event);
+    }
+
+    fn typeErasedDrawFn(
+        ptr: *anyopaque,
+        ctx: vxfw.DrawContext,
+    ) std.mem.Allocator.Error!vxfw.Surface {
+        const self: *ProjectPicker = @ptrCast(@alignCast(ptr));
+        return try self.draw(ctx);
+    }
+
+    pub fn widget_builder(ptr: *const anyopaque, idx: usize, _: usize) ?vxfw.Widget {
+        const self: *const ProjectPicker = @ptrCast(@alignCast(ptr));
+        if (idx >= self.filtered.items.len) return null;
+
+        return self.filtered.items[idx].widget();
+    }
+
+    pub fn on_change(maybe_ptr: ?*anyopaque, _: *vxfw.EventContext, str: []const u8) anyerror!void {
+        const ptr = maybe_ptr orelse return;
+        const self: *ProjectPicker = @ptrCast(@alignCast(ptr));
+
+        // Clear the filtered list and the arena.
+        self.filtered.clearAndFree();
+        _ = self.arena.reset(.free_all);
+
+        const allocator = self.arena.allocator();
+
+        // If there is text in the search box we only render items that contain the search string.
+        // Otherwise we render all the items.
+        if (str.len > 0) {
+            for (self.list.items) |item| {
+                if (std.mem.containsAtLeast(u8, item.text, 1, str)) {
+                    // FIXME: add more spans that highlight the matching text.
+                    var spans = std.ArrayList(vxfw.RichText.TextSpan).init(allocator);
+                    const span: vxfw.RichText.TextSpan = .{ .text = item.text };
+                    try spans.append(span);
+                    try self.filtered.append(.{ .text = spans.items });
+                }
+            }
+        } else {
+            for (self.list.items) |line| {
+                var spans = std.ArrayList(vxfw.RichText.TextSpan).init(allocator);
+                const span: vxfw.RichText.TextSpan = .{ .text = line.text };
+                try spans.append(span);
+                try self.filtered.append(.{ .text = spans.items });
+            }
+        }
+
+        self.list_view.scroll.top = 0;
+        self.list_view.scroll.offset = 0;
+        self.list_view.cursor = 0;
+    }
+
+    pub fn on_submit(maybe_ptr: ?*anyopaque, ctx: *vxfw.EventContext, _: []const u8) anyerror!void {
+        const ptr = maybe_ptr orelse return;
+        const self: *ProjectPicker = @ptrCast(@alignCast(ptr));
+
+        const arena = self.arena.allocator();
+        self.result.clearAndFree();
+
+        // 1. We want to quit on every submit, even ones that fail.
+
+        ctx.quit = true;
+
+        // 2. Get the selected item.
+
+        if (self.list_view.cursor >= self.filtered.items.len) return;
+
+        var selected_item = std.ArrayList(u8).init(arena);
+        defer selected_item.deinit();
+
+        for (self.filtered.items[self.list_view.cursor].text) |span| {
+            try selected_item.appendSlice(span.text);
+        }
+
+        // 3. If we can find a home directory replace any `~` in the chosen path with the path to
+        //    the home directory.
+
+        const home_path = kf.getPath(arena, .home) catch null;
+        if (home_path) |home| {
+            const replace_len = std.mem.replacementSize(u8, selected_item.items, "~", home);
+            const result = try arena.alloc(u8, replace_len);
+
+            _ = std.mem.replace(
+                u8,
+                selected_item.items,
+                "~",
+                home,
+                result,
+            );
+
+            try self.result.appendSlice(result);
+            return;
+        }
+
+        // 4. Otherwise just return the chosen item unmodified.
+
+        try self.result.appendSlice(selected_item.items);
+    }
+};
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    const allocator = gpa.allocator();
+
+    var app = try vxfw.App.init(allocator);
+    errdefer app.deinit();
+
+    const picker = try allocator.create(ProjectPicker);
+    defer allocator.destroy(picker);
+
+    picker.* = .{
+        .list = std.ArrayList(vxfw.Text).init(allocator),
+        .filtered = std.ArrayList(vxfw.RichText).init(allocator),
+        .list_view = .{
+            .children = .{
+                .builder = .{
+                    .userdata = picker,
+                    .buildFn = ProjectPicker.widget_builder,
+                },
+            },
+        },
+        .text_field = .{
+            .buf = vxfw.TextField.Buffer.init(allocator),
+            .unicode = &app.vx.unicode,
+            .userdata = picker,
+            .onChange = ProjectPicker.on_change,
+            .onSubmit = ProjectPicker.on_submit,
+        },
+        .arena = std.heap.ArenaAllocator.init(allocator),
+        .result = std.ArrayList(u8).init(allocator),
+    };
+    defer picker.text_field.deinit();
+    defer picker.list.deinit();
+    defer picker.filtered.deinit();
+    defer picker.arena.deinit();
+    defer picker.result.deinit();
+
+    // 1. Open the ~/.config directory, or equivalent on Windows.
+
+    const config_dir = kf.open(
+        allocator,
+        .local_configuration,
+        .{ .access_sub_paths = true },
+    ) catch |err| {
+        std.log.err("failed to open config directory: {}", .{err});
+        std.process.exit(74); // EX_IOERR from sysexits.h - I/O error on file.
+    };
+
+    if (config_dir) |config| {
+        // 2. Read the contents of the ~/.config/project-picker/projects file.
+
+        const pp_dir = config.makeOpenPath("project-picker", .{}) catch |err| {
+            std.log.err("failed to open project-picker config directory: {}", .{err});
+            std.process.exit(74); // EX_IOERR from sysexits.h - I/O error on file.
+        };
+
+        const projects_file = pp_dir.createFile(
+            "projects",
+            .{ .truncate = false, .read = true },
+        ) catch |err| {
+            std.log.err("failed to load project-picker project file: {}", .{err});
+            std.process.exit(74); // EX_IOERR from sysexits.h - I/O error on file.
+        };
+
+        const projects = projects_file.reader().readAllAlloc(
+            allocator,
+            std.math.maxInt(usize),
+        ) catch |err| {
+            std.log.err("failed to read project-picker project file: {}", .{err});
+            std.process.exit(74); // EX_IOERR from sysexits.h - I/O error on file.
+        };
+        defer allocator.free(projects);
+
+        // 3. Parse the lines of the file and add them to the available items in the project picker.
+
+        var it = std.mem.tokenizeScalar(u8, projects, '\n');
+        while (it.next()) |token| {
+            try picker.list.append(.{ .text = token });
+        }
+
+        // 4. Run the picker.
+
+        try app.run(picker.widget(), .{});
+        app.deinit();
+
+        // 5. If no selection was made exit with $status == 1.
+
+        if (picker.result.items.len == 0) {
+            std.process.exit(1);
+        }
+
+        // 6. Print the chosen path to STDOUT.
+
+        const stdout = std.io.getStdOut().writer();
+        nosuspend stdout.print("{s}", .{picker.result.items}) catch |err| {
+            std.log.err("{s}", .{@errorName(err)});
+            std.process.exit(74); // EX_IOERR from sysexits.h - I/O error on file.
+        };
+    } else {
+        std.log.err("failed to open config directory", .{});
+        std.process.exit(74); // EX_IOERR from sysexits.h - I/O error on file.
+    }
+}
